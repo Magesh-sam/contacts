70d46874c73db707480c806d5a0640586e37f33b
MAGESHKANNAN ANNATHURAI <mageshkannanam@gmail.com>
2023-04-22 18:26:16 +0530

    removed unwanted imports

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import contactReducer from "./contactSlice";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
